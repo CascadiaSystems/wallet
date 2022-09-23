ba65392793f4c89c954cf0ab4e134bd58a129016
Credit-First <hanoyoshito@gmail.com>
2022-09-23 20:22:35 +0000

    fix: remove title and add access, create wallet buttons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,61 +3,37 @@
         <b-container>
             <b-row>
                 <b-col>
-                    <div class="home_wrapper">
-                        <h1>{{ $t('home.desc') }}</h1>
-                        <div class="login_wrapper">
-                            <div class="login_option">
-                                <header>
-                                    <div class="img_container">
-                                        <img
-                                            v-if="$root.theme === 'day'"
-                                            src="@/assets/diamond-primary.svg"
-                                            alt
-                                        />
-                                        <img v-else src="@/assets/diamond-primary-night.svg" alt />
-                                    </div>
-                                    <h2>{{ $t('home.access.title') }}</h2>
-                                    <p>{{ $t('home.access.desc') }}</p>
-                                </header>
-                                <div>
-                                    <router-link
-                                        data-cy="access"
-                                        to="/access"
-                                        class="ava_button button_primary submit_but"
-                                    >
-                                        {{ $t('home.access.submit') }}
-                                    </router-link>
-                                </div>
-                            </div>
-                            <div class="login_option">
-                                <header>
-                                    <div class="img_container">
-                                        <img
-                                            v-if="$root.theme === 'day'"
-                                            src="@/assets/diamond-secondary.png"
-                                            alt
-                                        />
-                                        <img
-                                            v-else
-                                            src="@/assets/diamond-secondary-night.svg"
-                                            alt
-                                        />
-                                    </div>
-                                    <h2>{{ $t('home.create.title') }}</h2>
-                                    <p>{{ $t('home.create.desc') }}</p>
-                                </header>
-                                <div>
-                                    <router-link
-                                        data-cy="create"
-                                        to="/create"
-                                        class="ava_button button_secondary submit_but"
-                                    >
-                                        {{ $t('home.create.submit') }}
-                                    </router-link>
-                                </div>
+                    <div class="access_card">
+                        <div class="menus">
+                            <AccountsFound class="accounts_menu"></AccountsFound>
+                            <div class="options">
+                                <router-link
+                                    data-cy="access"
+                                    to="/access"
+                                    class="menu_option button_primary"
+                                >
+                                    {{ $t('home.access.submit') }}
+                                    <ImageDayNight
+                                        day="/img/access_icons/day/access.svg"
+                                        night="/img/access_icons/night/access.svg"
+                                    ></ImageDayNight>
+                                </router-link>
+                                <router-link
+                                    data-cy="create"
+                                    to="/create"
+                                    class="menu_option button_primary"
+                                >
+                                    {{ $t('home.create.submit') }}
+                                    <ImageDayNight
+                                        day="/img/access_icons/day/create.svg"
+                                        night="/img/access_icons/night/create.svg"
+                                    ></ImageDayNight>
+                                </router-link>
                             </div>
                         </div>
-                        <ToS class="tos" style="align-self: center; margin: 30px !important"></ToS>
+
+                        <ToS style="margin: 20px !important"></ToS>
+                        <router-link to="/" class="link">{{ $t('access.cancel') }}</router-link>
                     </div>
                 </b-col>
             </b-row>
@@ -79,177 +55,77 @@ export default class Home extends Vue {}
 
 <style scoped lang="scss">
 @use "../main";
+@use '/src/components/Access/menu';
 
-.home {
-    padding-top: 100px;
-    /*background-color: #fff;*/
+.access_card {
+    margin: 0px auto;
     display: flex;
-    justify-content: center;
-    align-items: flex-start;
-    position: relative;
-
-    a {
-        margin: 10px;
-        text-align: center;
-        display: block;
-    }
-
-    .home_wrapper {
-        display: flex;
-        flex-direction: column;
-        align-content: center;
-        justify-content: center;
-
-        h1 {
-            text-align: center;
-            font-size: 22px;
-            font-weight: 400;
-        }
-
-        .login_wrapper {
-            margin-top: 60px;
-            display: grid;
-            grid-template-columns: 1fr 1fr;
-            column-gap: main.$container-padding;
-
-            .login_option {
-                display: flex;
-                flex-direction: column;
-                border-radius: 2px;
-                align-items: flex-start;
-                justify-content: space-between;
-                background-color: var(--bg-light);
-                padding: 60px 90px main.$container-padding main.$container-padding;
-
-                header {
-                    margin-bottom: 60px;
-
-                    img {
-                        width: 89px;
-                        height: 89px;
-                        max-height: none;
-                    }
-
-                    h2 {
-                        padding-top: main.$s-size;
-                        font-family: 'DM Sans', sans-serif;
-                        font-size: main.$s-size;
-                        text-transform: uppercase;
-                        color: var(--primary-color-light);
-                    }
-
-                    p {
-                        margin-top: 10px !important;
-                        font-size: main.$l-size;
-                    }
-                }
-
-                a {
-                    margin: 0;
-                }
-            }
-        }
-    }
-}
-
-.tos {
-    margin-top: 14px !important;
-}
-
-.submit_but {
-    width: max-content;
-}
-/* ==========================================
-   Nav
-   ========================================== */
-
-@include main.night-mode {
-}
-.logo {
-    margin-bottom: 30px;
+    flex-direction: column;
+    align-items: center;
 }
 
 img {
-    max-height: 50px;
-    object-fit: contain;
+    width: main.$img-size;
+    height: main.$img-size;
+    margin-bottom: main.$vertical-padding;
 }
 
-@include main.medium-device {
-    .login_option {
-        padding: 30px 40px !important;
-        p {
-            font-size: 1.4rem !important;
-        }
+h1 {
+    font-size: main.$l-size;
+    font-weight: 400;
+}
+
+hr {
+    max-width: 67% !important;
+    margin: main.$vertical-padding auto 0;
+    color: main.$primary-color-light;
+    opacity: 0.2;
+}
+
+.accounts_menu {
+    margin-bottom: 30px;
+}
+
+.options {
+    display: flex;
+    flex-direction: column;
+}
+
+.menu_option {
+    justify-content: space-between;
+    align-items: center;
+    img {
+        width: 24px;
+        height: 24px;
+        margin: 0;
+        object-fit: contain;
     }
+}
+
+.menus {
+    width: 440px;
+    max-width: 100%;
+    margin-top: 1em;
 }
 
 @include main.mobile-device {
-    .auth {
-        border-radius: 0;
-        box-shadow: none;
+    img {
+        width: main.$img-size-mobile;
+        height: main.$img-size-mobile;
+        margin-bottom: main.$vertical-padding-mobile;
     }
 
-    .menu_option {
-        padding: 5vh 12px;
-    }
-    .menu_option button {
-        width: 100%;
-        padding: 8px;
+    h1 {
+        font-size: main.$l-size-mobile;
     }
 
-    .imgcover {
-        display: none;
+    .card {
+        padding: main.$container-padding-mobile;
     }
 
-    .home {
-        .home_wrapper {
-            h1 {
-                font-size: main.$xl-size-mobile;
-            }
-
-            .login_wrapper {
-                grid-template-columns: none;
-                display: flex;
-                flex-direction: column;
-
-                .login_option {
-                    margin-bottom: main.$vertical-padding;
-                    padding: 30px 15px;
-                    align-items: center;
-
-                    header {
-                        display: flex;
-                        flex-direction: column;
-                        align-items: center;
-
-                        margin-bottom: 30px;
-
-                        img {
-                            width: 40px;
-                            height: 40px;
-                        }
-
-                        h2 {
-                            padding-top: main.$s-size-mobile;
-                            font-size: main.$s-size-mobile;
-                        }
-
-                        p {
-                            margin-top: 10px !important;
-                            font-size: main.$l-size-mobile;
-                            text-align: center;
-                        }
-                    }
-
-                    a {
-                        margin: 0;
-                    }
-                }
-            }
-        }
+    .options {
+        display: block;
+        grid-template-columns: none;
     }
-}
-
-@media only screen and (max-width: 600px) {
 }
 </style>
